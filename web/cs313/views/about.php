3a5ec3b216b8b76beb3afbe21bfac84e2577f5c4
Dustin Myers <mye15011@byui.edu>
2020-04-29 22:23:20 -0600

    added CSS

--- web/cs313/views/about.php
+++ web/cs313/views/about.php
@@ -25,8 +25,8 @@
         <p>My life is not complete with just fun and work though. At the end of my second semester at BYUI, I got engaged to my now wife. We were married in september of 2018 and a year later welcomed a baby girl into our family. I love to spend time with them and watch my daughter develop and grow. We are building our lives in Evanston. We rent and repair my boss' rental house. Life for me is always an adventure.</p>
         <img src="./images/family.jpg" alt="Family">
     </main>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </body>
 </html>
