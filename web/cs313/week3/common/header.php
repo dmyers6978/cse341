ea2f2753963a2aad7362674f835436d2a19cdcc8
Dustin Myers <mye15011@byui.edu>
2020-05-05 16:37:55 -0600

    Week 3 assignment

--- web/cs313/week3/common/header.php
+++ web/cs313/week3/common/header.php
@@ -2,7 +2,7 @@
 <a href="/cs313/week3/index.php?action=viewCart">  <div class="icon-cart" style="clear: left; float: left">
             <div class="cart-line-1" style="background-color: #2CC3B5"></div>
             <div class="cart-line-2" style="background-color: #2CC3B5"></div>
-            <div class="items">10</div>
+            <div class="items"><?php if(!$_SESSION['cart']){$_SESSION['cart'] = array();}echo count($_SESSION['cart']);?></div>
             <div class="cart-wheel" style="background-color: #2CC3B5"></div>
           </div></a>
 </header>
