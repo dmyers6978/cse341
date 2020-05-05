302155c41c3cf2f5b76312f506f67965bcb95941
Dustin Myers <mye15011@byui.edu>
2020-05-05 16:46:26 -0600

    Week 3 assignment

--- web/cs313/week3/index.php
+++ web/cs313/week3/index.php
@@ -1,4 +1,7 @@
 <?php
+if(!$_SESSION){
+    session_start();
+}
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
 if(!$action){
     $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
@@ -8,15 +11,26 @@ switch($action){
 
     case 'viewCart':
         include './views/cart.php';
+        exit;
     break;
 
     case 'checkout':
         include './views/checkout.php';
+        exit;
     break;
 
     case 'confirmation':
         include './views/confirmation.php';
+        exit;
     break;
+
+    case 'addToCart';
+    $itemTitle = filter_input(INPUT_GET, 'itemTitle', FILTER_SANITIZE_STRING);
+    $itemPrice = filter_input(INPUT_GET, 'itemPrice', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    array_push(array('itemTitle'=>$itemTitle,'itemPrice'=>$itemPrice));
+    header('location: ./index.php');
+    exit;
+break;
 
     default:
     include './views/viewItems.php';
