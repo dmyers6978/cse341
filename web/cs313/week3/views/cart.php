d605fb97a7eeaa07178d7d1cb3ee6cf413042e31
Dustin Myers <mye15011@byui.edu>
2020-05-05 17:00:15 -0600

    Week 3 assignment

--- web/cs313/week3/views/cart.php
+++ web/cs313/week3/views/cart.php
@@ -12,7 +12,25 @@
 <body>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/cs313/week3/common/header.php';?>
     <main>
-        
+        <fieldset>
+            <legend>Cart Items</legend>
+            <table>
+                <thead>
+                    <tr><td>Item Title</td><td>Price</td></tr>
+                </thead>
+                <tbody>
+                <?php foreach($_SESSION['cart'] as $item){
+                        echo "<tr><td>$item[itemTitle]</td><td>$item[itemPrice]</td></tr>";
+                    }?>
+                    <tr><td></td><td><?php $total = 0;
+                    foreach($_SESSION['cart'] as $item){
+                        $total = $total + $item['itemPrice'];
+                    }
+                    echo "$$total";
+                        ?></td></tr>
+                </tbody>
+            </table>
+        </fieldset>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
