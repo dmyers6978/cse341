2e45e1ea0edcbcdd15a6ef38beda9e198196af95
Dustin Myers <mye15011@byui.edu>
2020-05-07 20:26:47 -0600

    Week 3 assignment

--- web/cs313/views/assignments.php
+++ web/cs313/views/assignments.php
@@ -16,7 +16,7 @@
         <?php include './common/nav.php';?>
     </nav>
     <main>
-        <p>Coming Soon</p>
+        <p><a href="https://sheltered-harbor-59493.herokuapp.com/cs313/week3/">Week 3 - Shopping Cart</a></p>
     </main>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
