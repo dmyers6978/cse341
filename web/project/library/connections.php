1102f6bf317618265f855c431de2735c0a7079d5
Dustin Myers <mye15011@byui.edu>
2020-05-21 15:56:39 -0600

    initial push

--- web/project/library/connections.php
+++ web/project/library/connections.php
@@ -1,7 +1,7 @@
 <?php
 $db = parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
+$db = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["host"],
     $db["port"],
@@ -9,20 +9,27 @@ $pdo = new PDO("pgsql:" . sprintf(
     $db["pass"],
     ltrim($db["path"], "/")
 ));
-// try
-// {
-//   $user = 'postgres';
-//   $password = 'password';
-//   $db = new PDO('pgsql:host=localhost;dbname=db8cjsi2qqbfk3', $user, $password);
+try
+{
+    $db = parse_url(getenv("DATABASE_URL"));
 
-//   // this line makes PDO give us an exception when there are problems,
-//   // and can be very helpful in debugging! (But you would likely want
-//   // to disable it for production environments.)
-//   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//   echo "Connected.";
-// }
-// catch (PDOException $ex)
-// {
-//   echo 'Error!: ' . $ex->getMessage();
-//   die();
-// }
+    $db = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+
+  // this line makes PDO give us an exception when there are problems,
+  // and can be very helpful in debugging! (But you would likely want
+  // to disable it for production environments.)
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected.";
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
