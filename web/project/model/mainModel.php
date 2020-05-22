5a56e24deb7b991e63c16703b54b1a3399b3872b
Dustin Myers <mye15011@byui.edu>
2020-05-21 20:07:13 -0600

    Update

--- web/project/model/mainModel.php
+++ web/project/model/mainModel.php
@@ -3,7 +3,7 @@ require_once '/app/web/project/library/connections.php';
 
 function getServices(){
     $db = dbConnect();
-    $sql = 'SELECT * FROM services;';
+    $sql = 'SELECT * FROM services ORDER BY servicePrice ASC, serviceName ASC;';
     $stmt = $db->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
