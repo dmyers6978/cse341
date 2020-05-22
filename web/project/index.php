5f751cadaefa8b29137c1e1bf6fd5c3bf6fcdff6
Dustin Myers <mye15011@byui.edu>
2020-05-21 19:37:36 -0600

    Update

--- web/project/index.php
+++ web/project/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once './model/mainModel.php';
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
 if(!$action){
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
