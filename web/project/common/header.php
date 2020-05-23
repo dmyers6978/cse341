7f94542ae169ec710b36efd88d47f1b8c10e8dcf
Dustin Myers <mye15011@byui.edu>
2020-05-23 12:00:08 -0600

    project update

--- web/project/common/header.php
+++ web/project/common/header.php
@@ -1,2 +1,6 @@
-<a href="./?action=login">Back for more? Login.</a>
-<a href="./?action=register">First time around? Sign up.</a>
+<?php if(!$_SESSION['loggedIn']){
+    echo '<a href="./?action=login">Back for more? Login.</a>
+    <a href="./?action=register">First time around? Sign up.</a>';
+} else{
+    echo "<span>Welcome back $_SESSION[userData][userfirstname] | </span><a href='./?action=logout'>Log Out</a>";
+}
