f104631b316984979eb3370bb700fcd09ecb7617
Dustin Myers <mye15011@byui.edu>
2020-05-22 21:31:53 -0600

    project

--- web/project/index.php
+++ web/project/index.php
@@ -13,7 +13,7 @@ switch($action){
         $table = '';
         $services = getServices();
         foreach($services as $service){
-            $table .= "<tr><td>$service[servicename]</td><td>$service[servicetime]</td><td>$service[serviceprice]</td><td><input type='radio' name='$service[serviceprice]' onclick='addCost(this)'></td></tr>";
+            $table .= "<tr><td>$service[servicename]</td><td>$service[servicetime]</td><td><label for='$service[serviceid]'>$service[serviceprice]</label>&nbsp;<input type='checkbox' id='$service[serviceid]'></td></tr>";
         }
         include './views/contact.php';
         exit;
