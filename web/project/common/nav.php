f5c488930958ca635007701c4e5a6e7d010c3055
Dustin Myers <mye15011@byui.edu>
2020-05-22 21:06:11 -0600

    Update

--- web/project/common/nav.php
+++ web/project/common/nav.php
@@ -1,3 +1,6 @@
 <ul>
-
+    <li><a href="./">Home</a></li>
+    <li><a href="./?action=contact">Contact Me</a></li>
+    <li><a href="./?action=status">Bike Status</a></li>
+    <li><a href="./?action=about">About Me</a></li>
 </ul>
