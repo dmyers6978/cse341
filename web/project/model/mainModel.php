a09206bf46d589a77cfc1ce7ab5ae4e99a0d9983
Dustin Myers <mye15011@byui.edu>
2020-05-30 15:24:46 -0600

    changes

--- web/project/model/mainModel.php
+++ web/project/model/mainModel.php
@@ -39,7 +39,7 @@ function checkUserEmail($email){
 
 function getAllJobs(){
     $db = dbConnect();
-    $sql = 'SELECT * FROM jobs JOIN status USING(statusid) JOIN user USING(userid);';
+    $sql = 'SELECT * FROM jobs JOIN status USING(statusid) JOIN users USING(userid);';
     $stmt = $db->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
