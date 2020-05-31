fef200b4ec56a77335e474187edf9ab76278e270
Dustin Myers <mye15011@byui.edu>
2020-05-30 19:42:08 -0600

    update

--- web/project/views/home.php
+++ web/project/views/home.php
@@ -5,6 +5,7 @@
   <title>[Name here] | Home</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" type="text/css" href="./style/common.css">
+  <link rel="stylesheet" type="text/css" href="./style/main.css">
 </head>
 <body>
     <header>
@@ -18,22 +19,16 @@
             echo $_SESSION['message'];
         }
         ?>
-    <section>
-        <figure><img src="" alt="repair"></figure>
-        <figcaption><a href="./?action=contact">Need a repair?</a></figcaption>
-    </section>
-    <section>
-        <figure><img src="" alt="status"></figure>
-        <figcaption><a href="./?action=status">Check Status</a></figcaption>
-    </section>
-    <section>
-        <figure><img src="" alt="FB"></figure>
-        <figcaption><a href="https://www.facebook.com/groups/606194506596284">Love Mountain biking?</a></figcaption>
-    </section>
-    <section>
-        <figure><img src="" alt="About"></figure>
-        <figcaption><a href="./?action=about">About me</a></figcaption>
-    </section>
+        <div>
+        <img src="" alt="repair" id="first">
+            <section id="second"><a href="./?action=contact"><p>Hit the trail too hard?</p><p>I have the tools and experience to get you back on the trail.</p></a></section>
+            <section id="third"><a href="./?action=status"><p>Already dropped off your bike?</p><p>Go here to see the status of your bike.</p></a></section>
+            <img src="" alt="status" id="fourth">
+            <img src="" alt="FB" id="fifth">
+            <section id="sixth"><a href="https://www.facebook.com/groups/606194506596284t"><p>Love Mountain biking?</p><p>So do I. Join my facebook group to find others as well.</p></a></section>
+            <section id="seventh"><a href="./?action=about"><p>Still skeptical?</p><p>Click here to know more about my origins and backstory.</p></a></section>
+            <img src="" alt="About" id="eighth">
+    </div>
     </main>
     <script src="./scripts/navButton.js"></script>
 </body>
