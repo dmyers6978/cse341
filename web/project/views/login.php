f4e8e3d7db32491209481cabc81665c7e60a7dca
Dustin Myers <mye15011@byui.edu>
2020-06-02 22:09:38 -0600

    update

--- web/project/views/login.php
+++ web/project/views/login.php
@@ -5,16 +5,21 @@
   <title>[Name here] | Home</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" type="text/css" href="./style/common.css">
+  <link rel="stylesheet" type="text/css" href="./style/login.css">
 </head>
 <body>
     <main>
     <section>
       <?php if(isset($_SESSION['message'])){echo $_SESSION['message'];}?>
         <form action="./" method="post">
+        <div>
         <label for="email">Email: </label>
         <input type="email" name="email" id="email" required>
+        </div>
+        <div>
         <label for="password">Password: </label>
         <input type="password" name="password" id="password" required>
+        </div>
         <input type="hidden" name="action" value="Login">
         <input type="submit" value="Login">
     </form>
