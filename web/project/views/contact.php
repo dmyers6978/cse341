da40eebba0a46bc9e83d1ef68a2dfed488e4c30a
Dustin Myers <mye15011@byui.edu>
2020-06-03 21:46:19 -0600

    update

--- web/project/views/contact.php
+++ web/project/views/contact.php
@@ -31,12 +31,18 @@
     </table>
     <p>*Prices are estimates. Depending on situation or parts required, prices are subject to change.</p>
 </section>
+<div>
     <section>
     <h1>Contact me for more info</h1>  
     <p>1213 Center St. Rear</p>
-    <p>Evanston WY 82930</p><a href="tel:30077995311">Call Me</a>
+    <p>Evanston WY 82930</p>
+    <div>
+    <a href="tel:30077995311">Call Me</a>
     <a href="./?action=email">Email Me</a>
-  </section>
+    </div>
+</section>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d749.7147251305189!2d-110.97017917079515!3d41.268408006716584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDE2JzA2LjMiTiAxMTDCsDU4JzEwLjciVw!5e0!3m2!1sen!2sus!4v1591239639417!5m2!1sen!2sus" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</div>
     </main>
     <script src="./scripts/cost.js"></script>
     <script src="./scripts/navButton.js"></script>
