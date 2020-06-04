0432c86a127b633a618d98b7376e55ea8fa1524f
Dustin Myers <mye15011@byui.edu>
2020-06-03 22:19:29 -0600

    update

--- web/project/common/nav.php
+++ web/project/common/nav.php
@@ -5,8 +5,8 @@
   <div class="bar3"></div>
 </div>
 <ul id="topNav" class="hide">
-    <li><a href="./">Home</a></li>
-    <li><a href="./?action=contact">Contact Me</a></li>
-    <li><a href="./?action=status">Bike Status</a></li>
-    <li><a href="./?action=about">About Me</a></li>
+    <li <?php if(!$action){ echo ' class="active"';}?>><a href="./">Home</a></li>
+    <li <?php if($action === "contact"){ echo ' class="active"';}?>><a href="./?action=contact">Contact Me</a></li>
+    <li <?php if($action === "status"){ echo ' class="active"';}?>><a href="./?action=status">Bike Status</a></li>
+    <li <?php if($action === "about"){ echo ' class="active"';}?>><a href="./?action=about">About Me</a></li>
 </ul>
