2cb526ffea3c1c18952fb717747e974a175ccf99
Dustin Myers <mye15011@byui.edu>
2021-01-14 20:55:53 -0700

    week2

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,4 +1,4 @@
 <ul>
-    <li><a href="/" <? if(!$action){echo "class='active'";}?>>Home</a></li>
-    <li><a href="/?action=assignments"<? if($action == "assignments"){echo "class='active'";}?>>Assignments</a></li>
+    <li <? if(!$action){echo "class='active'";}?>><a href="/">Home</a></li>
+    <li <? if($action == "assignments"){echo "class='active'";}?>><a href="/?action=assignments">Assignments</a></li>
 </ul>
