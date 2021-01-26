bd79848225c5e40598819c6bd0be2a3c89735604
Dustin Myers <mye15011@byui.edu>
2021-01-25 18:54:39 -0700

    DB and architecture setup

--- web/inventory/index.php
+++ web/inventory/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once './models/mainModel.php';
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 if(!$action){
