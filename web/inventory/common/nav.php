cb4be672b68f4675de1ee7bf578d44ba8a855a8e
Dustin Myers <mye15011@byui.edu>
2021-02-05 20:07:05 -0700

    week 5

--- web/inventory/common/nav.php
+++ web/inventory/common/nav.php
@@ -2,4 +2,5 @@
     <li <? if(!$action){echo "class='active'";}?>><a href="./">Home</a></li>
     <li <? if($action == "invManager"){echo "class='active'";}?>><a href="./?action=invManager">Inventory Manager</a></li>
     <li <? if($action == "viewInv"){echo "class='active'";}?>><a href="./?action=viewInv">View Inventory</a></li>
+    <li <? if($action == "itemManager"){echo "class='active'";}?>><a href="./?action=itemManager">Item Manager</a></li>
 </ul>
