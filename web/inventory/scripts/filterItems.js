320dff3c70390c1b6634921c43e6b2362d72537f
Dustin Myers <mye15011@byui.edu>
2021-02-06 15:22:49 -0700

    week 5

--- web/inventory/scripts/filterItems.js
+++ web/inventory/scripts/filterItems.js
@@ -1,12 +1,12 @@
 var body = document.getElementsByTagName("TBODY")[0];
 var rows = body.getElementsByTagName("TR");
 function filterItems(){
-var search = document.getElementById('itemSearch').value.toUpper();
+var search = document.getElementById('itemSearch').value.toUpperCase();
 console.log(rows[0].children[0].innerHTML.indexOf(search));
 for(var i = 0; i < rows.length; i++){
 if(!search){
 rows[i].style.display = 'table-row';
-} else if(rows[i].children[0].innerHTML.toUpper().indexOf(search) == -1 && rows[i].children[1].innerHTML.toUpper().indexOf(search) == -1){
+} else if(rows[i].children[0].innerHTML.toUpperCase().indexOf(search) == -1 && rows[i].children[1].innerHTML.toUpperCase().indexOf(search) == -1){
 rows[i].style.display = 'none';
 } else{
 rows[i].style.display = 'table-row';
