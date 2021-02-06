24098adae86cf6e53449470bfb313d67ea52500d
Dustin Myers <mye15011@byui.edu>
2021-02-06 15:25:54 -0700

    week 5

--- web/inventory/library/functions.php
+++ web/inventory/library/functions.php
@@ -12,7 +12,7 @@ function buildItemSelect(){
 function buildInvTable(){
     $inventory = getInv();
     if($inventory){
-    $table = "<label for='itemSearch'><input type='text' id='itemSearch' onkeyup='filterItems()'>🔍</label><table><thead><tr><th>Item</th><th>Quantity</th></tr></thead><tbody>";
+    $table = "<label for='itemSearch' class='itemSearch'><input type='text' id='itemSearch' onkeyup='filterItems()'>🔍</label><table><thead><tr><th>Item</th><th>Quantity</th></tr></thead><tbody>";
     foreach($inventory as $item){
         $table .= "<tr><td>$item[itemname]</td><td>$item[quantity]</td></tr>";
     }
