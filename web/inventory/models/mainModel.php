a97d92c19823ed7c906f3feda93b55864c59ae4c
Dustin Myers <mye15011@byui.edu>
2021-02-10 17:53:57 -0700

    Week6

--- web/inventory/models/mainModel.php
+++ web/inventory/models/mainModel.php
@@ -108,7 +108,7 @@ function logAction($itemId, $quantity){
 
 function getLogs(){
     $db = dbConnect();
-    $sql = "SELECT *, TO_CHAR(datetime, 'DD-MM-YYYY HH:MI:SS AM') AS dateTime FROM log JOIN items USING(itemId) ORDER BY datetime DESC";
+    $sql = "SELECT *, TO_CHAR(datetime, 'DD-MM-YYYY HH:MI:SS AM') AS dateTime FROM log JOIN items USING(itemId) ORDER BY log.datetime DESC";
     $stmt = $db->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
